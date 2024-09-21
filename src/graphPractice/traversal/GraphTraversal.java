ce6846f22a2a315c4213fce5a821c953cb84f748
Shivam Kumar <shivamkumar@Shivams-MacBook-Pro.local>
2024-09-22 01:36:46 +0530

    bfs dfs

--- src/graphPractice/traversal/GraphTraversal.java
+++ src/graphPractice/traversal/GraphTraversal.java
@@ -1,17 +1,55 @@
 package graphPractice.traversal;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class GraphTraversal {
 
-    public void dfs(ArrayList<ArrayList<Integer>> graph,int v,boolean[] vis){
-        vis[v]=true;
-        System.out.print(v+"  ");
+    private void dfsUtil(ArrayList<ArrayList<Integer>> adjacencyGraph,int src,boolean[] vis){
+        vis[src]=true;
+        System.out.print(src+"  ");
 
-        for(int u:graph.get(v)){
-            if(!vis[u]){
-                dfs(graph,u,vis);
+        for(int neighbour:adjacencyGraph.get(src)){
+            if(!vis[neighbour]){
+                dfsUtil(adjacencyGraph,neighbour,vis);
             }
         }
+    }
+
+    public void dfs(ArrayList<ArrayList<Integer>> adjacencyGraph){
+        int V=adjacencyGraph.size();
+        boolean[] vis=new boolean[V];
+
+        for(int node=0;node<V;node++){
+            if(!vis[node]){
+                dfsUtil(adjacencyGraph,node,vis);
+            }
+        }
+    }
+
+    public void bfs(ArrayList<ArrayList<Integer>> adjacencyGraph){
+        int V=adjacencyGraph.size();
+        boolean[] vis=new boolean[V];
+
+
+    }
+
+    private void bfsUtil(ArrayList<ArrayList<Integer>> adjacencyGraph,int src,boolean[] vis){
+
+        Queue<Integer> q=new LinkedList<>();
+        q.offer(src);
+        vis[src]=true;
+
+        while(!q.isEmpty()){
+            int node=q.poll();
+            for(int neighbour:adjacencyGraph.get(node)){
+                if(!vis[neighbour]){
+                    q.offer(neighbour);
+                    vis[neighbour]=true;
+                }
+            }
+        }
+
     }
 }
